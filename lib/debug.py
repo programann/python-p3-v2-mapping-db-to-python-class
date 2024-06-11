e56c3c38e6567263cd1790e499f053a5109d4f40
Clarence Kinuthia <mclarence7311@gmail.com>
2024-06-11 23:09:06 +0300

    commit

--- lib/debug.py
+++ lib/debug.py
@@ -7,7 +7,7 @@ import ipdb
 def reset_database():
     Department.drop_table()
     Department.create_table()
-
+    
     Department.create("Payroll", "Building A, 5th Floor")
     Department.create("Human Resources", "Building C, East Wing")
     Department.create("Accounting", "Building B, 1st Floor")
